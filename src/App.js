d9bd883cef9b367193f8225cf00b92a857fc70a1
ricky3knowhere <ricky03senju@gmail.com>
2020-09-10 09:43:50 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostList from './components/PostLIst'
+import DeleteButton from './components/DeleteButton'
+import AddButton from './components/AddButton'
+import { Provider } from 'react-redux'
+import store from './store'
+import NewInput from './components/NewInput'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <PostList />
+        <AddButton />
+        <DeleteButton />
+        <NewInput />
+      </Provider>
     </div>
   );
 }
